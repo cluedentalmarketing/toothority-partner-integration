b6cd760ec6069633199f887767af7acc9bf69a48
Danielek Pałka <9382609+dpaanlka@users.noreply.github.com>
2019-09-26 11:34:10 -0500

    Cleaned up video stream selection block

--- muvi/muvi.js
+++ muvi/muvi.js
@@ -33,7 +33,6 @@ for (i = 0; i < muviplayers.length; i++) {
     muviplayers[i].setAttribute("oncontextmenu", "return false;");
     muviplayers[i].setAttribute("onClick", 'if (!this.hasAttribute("controls")) {this.setAttribute("controls", "controls"); this.play();}');
     muviplayers[i].setAttribute("ontouchstart", 'if (!this.hasAttribute("controls")) {this.setAttribute("controls", "controls"); this.play();}');
-    
     muviplayers[i].setAttribute("poster", "https://assets.muvidental.com/img/posters/" + videotopic + ".jpg");     // Not protected, no token needed
     
     
@@ -58,56 +57,43 @@ for (i = 0; i < muviplayers.length; i++) {
                 
     
     
-    if (muviplayers[i].canPlayType('application/vnd.apple.mpegurl')) {
-        
+    if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1 && !window.MSStream) {
+						// Safari specific code
             if (isiOS == true && osVersion[1] >= 11) {
                 // Send H265 master playlist to iOS 11+ Safari
-                muviplayers[i].src = streamPHP + '?l=1&c=hevc&v=' + videotopic; 
+                muviplayers[i].src = streamPHP + '?l=1&c=hevc&v=' + videotopic;
                 muviplayers[i].addEventListener('canplay', function() {
                     // muviplayers[i].play();
                 });
                 // document.getElementById("infodiv").innerHTML = "Native H.265 (A)";
+								console.log('Break 1');
             } else if (isMac == true && osVersion[1] >= 13) {
                 // Send H265 master playlist to Mac OS X 13+ Safari
-                muviplayers[i].src = streamPHP + '?l=1&c=hevc&v=' + videotopic; 
+                muviplayers[i].src = streamPHP + '?l=1&c=hevc&v=' + videotopic;
                 muviplayers[i].addEventListener('canplay', function() {
                     // muviplayers[i].play();
                 });
                 // document.getElementById("infodiv").innerHTML = "Native H.265 (B)";
+								console.log('Break 2');
             } else {
                 // Send H264 master playlist to older iDevices
-                muviplayers[i].src = streamPHP + '?l=1&v=' + videotopic; 
+                muviplayers[i].src = streamPHP + '?l=1&v=' + videotopic;
                 muviplayers[i].addEventListener('canplay', function() {
                     // muviplayers[i].play();
                 });
                 // document.getElementById("infodiv").innerHTML = "Native H.264 (C)";
+								console.log('Break 3');
             }
-            
+
     } else if (Hls.isSupported()) {
+				// Chrome, Firefox, IE, etc...
         var hls = new Hls(config);
-            
-        // Check if browser is Safari. Leaving as a fallback in case the above check fails. Will test more and possibly remove in future.
-        if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1 && !window.MSStream) {
-            
-            if (isiOS == true && osVersion[1] >= 11) {
-                // Send H265 master playlist to iOS 11+ Safari
-                hls.loadSource(streamPHP + '?l=1&c=hevc&v=' + videotopic);
-                // document.getElementById("infodiv").innerHTML = "hls.js H.265 (D)";
-            } else if (isMac == true && osVersion[1] >= 13) {
-                // Send H265 master playlist to Mac OS X 13+ Safari
-                hls.loadSource(streamPHP + '?l=1&c=hevc&v=' + videotopic);
-                // document.getElementById("infodiv").innerHTML = "hls.js H.265 (E)";
-            } else {
-                // Send H264 master playlist to older iDevices
-                hls.loadSource(streamPHP + '?l=1&&v=' + videotopic);
-                // document.getElementById("infodiv").innerHTML = "hls.js H.265 (F)";
-            }
-            
-        } else {
-            // Send H264 master playlist to everyone else
-            hls.loadSource(streamPHP + '?l=1&v=' + videotopic);
-            // document.getElementById("infodiv").innerHTML = "hls.js H.264 (G)";
-        }
+
+        // Send H264 master playlist to everyone else
+        hls.loadSource(streamPHP + '?l=1&v=' + videotopic);
+        // document.getElementById("infodiv").innerHTML = "hls.js H.264 (G)";
+				console.log('Break 7');
+
         hls.attachMedia(muviplayers[i]);
         hls.on(Hls.Events.MANIFEST_PARSED,function() {
             // video.play();
@@ -189,10 +175,3 @@ for (i = 0; i < muviplayers.length; i++) {
     // muviplayers[i].appendChild(ruTrack);
 
 }
-
-
-
-
-
-
-
